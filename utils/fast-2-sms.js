dced1d268fc5284532226f01771a7449ee916178
Amit Vadgama <amit.vadgama@1center.co>
2023-07-18 11:14:14 +0530

    new

--- utils/fast-2-sms.js
+++ utils/fast-2-sms.js
@@ -3,7 +3,7 @@ const sendSMS = async () => {
   const res = await axios.post("https://www.fast2sms.com/dev/bulkV2", {
     method: "POST",
     headers: {
-      "authorization": `Bearer L6WXPpCi1IDeG4xKnY8VEdhHFTJzU9vcOoumkl2tMrBgZAwSybqdI3OmAUX5PvzJEFCY4gnRQ89DGflh`,
+      "authorization": `Bearer ${process.env.FAST_2_SMS_API}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
